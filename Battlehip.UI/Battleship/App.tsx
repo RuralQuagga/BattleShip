06429c8a261562c28f998638afe3493b7a499982
RuralQuagga <dimas300997@gmail.com>
2025-05-04 11:16:50 +0300

    add redux

--- Battlehip.UI/Battleship/App.tsx
+++ Battlehip.UI/Battleship/App.tsx
@@ -1,15 +1,19 @@
 import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import MainNavigator from './navigation/MainNavigator';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 export default function App() {
   return (
+    <Provider store={store}>
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle="dark-content" />
       <NavigationContainer>
         <MainNavigator />
       </NavigationContainer>
     </SafeAreaView>    
+    </Provider>
   );
 }
 
